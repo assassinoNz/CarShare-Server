b7295246274a8daf1ca751d2eeaef0a85c9b0eec
assassino <nirmaldiaz@gmail.com>
2023-11-24 09:56:38 +0530

    Added: sampleConfig.ts

---
+++ etc/sampleConfig.ts
@@ -0,0 +1,12 @@
+export const SECRET_JWT = "Hello World;";
+export const URL_MONGO = "mongodb://localhost";
+export const PORT_EXPRESS = 8080;
+export const DB_MONGO = "carshare";
+
+export const URL_OSRM = "http://localhost:5000/route/v1/driving";
+
+export const HOST_POSTGRES = "localhost";
+export const USER_POSTGRES = "postgres";
+export const PASSWORD_POSTGRES = "password";
+export const DB_POSTGRES = "carshare";
+export const PORT_POSTGRES = 5432;
