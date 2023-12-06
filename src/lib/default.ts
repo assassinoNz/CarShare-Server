6fc2f3b5c16739f340bfa99cfd23a24a822850c2
assassino <nirmaldiaz@gmail.com>
2023-12-06 08:03:30 +0530

    Added: Doc comment

--- src/lib/default.ts
+++ src/lib/default.ts
@@ -1,4 +1,8 @@
 export const ID_ROLE = "100000000000000000000001";
 export const VALUE_SKIP = 0;
 export const VALUE_LIMIT = 10;
+
+/**
+ * Number of milliseconds in a month
+ */
 export const DATE_OFFSET = 2592000000;
